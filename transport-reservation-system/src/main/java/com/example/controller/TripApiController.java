7887bc31261d13a56a74bada56a558d7e6fbb55a
ayesh123 <ayesh9206@gmail.com>
2016-09-09 01:58:15 +0530

    second commit : editing Trip Class - preFinal

--- transport-reservation-system/src/main/java/com/example/controller/TripApiController.java
+++ transport-reservation-system/src/main/java/com/example/controller/TripApiController.java
@@ -10,7 +10,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
+import javax.print.DocFlavor;
 import javax.websocket.server.PathParam;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -56,5 +60,81 @@ public class TripApiController {
         td.setPassengers(trip.getPassengers());
         tdrep.save(td);
         return trip;
+    }
+
+    @Transactional(readOnly = true)
+    @RequestMapping(method = RequestMethod.GET,path = "/{id}")
+    @ResponseBody
+    public Trip getTripById(@PathVariable("id") String id){
+        TripData td = tdrep.findOne(Long.valueOf(id));
+        Trip trip = convertToTrip(td);
+        return trip;
+    }
+
+    private Trip convertToTrip(TripData td){
+        Trip trip = new Trip();
+        trip.sett_id(td.gett_id());
+        trip.sett_Date(td.gett_Date());
+        trip.sett_class(td.gett_class());
+        trip.sets_loc(td.gets_loc());
+        trip.sete_loc(td.gete_loc());
+        trip.setPassengers(td.getPassengers());
+        return trip;
+    }
+
+    @Transactional(readOnly = true)
+    @RequestMapping(method = RequestMethod.GET,path="/start/{s_loc}")
+    @ResponseBody
+    public List<Trip> getTripByStart(@PathVariable("s_loc") String s_loc){
+        List<TripData> byStart = tdrep.findByStart(s_loc);
+        return byStart.stream().map(this::convertToTrip).collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    @RequestMapping(method = RequestMethod.GET,path = "/end/{e_loc}")
+    @ResponseBody
+    public List<Trip> getTripByEnd(@PathVariable("e_loc") String e_loc){
+        List<TripData> byEnd = tdrep.findByEnd(e_loc);
+        return byEnd.stream().map(this::convertToTrip).collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    @RequestMapping(method = RequestMethod.GET,path = "/t_class/{t_class}")
+    @ResponseBody
+    public List<Trip> getTripByClass(@PathVariable("t_class") int t_class){
+        List<TripData> byClass = tdrep.findByClass(t_class);
+        return byClass.stream().map(this::convertToTrip).collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    @RequestMapping(method = RequestMethod.GET,path="/t_date/{t_date}")
+    @ResponseBody
+    public List<Trip> getTripByDate(@PathVariable("t_date") String t_date){
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date date;
+        try {
+            date = sdf.parse(t_date);
+            List<TripData> byDate = tdrep.findByDate(date);
+            return byDate.stream().map(this::convertToTrip).collect(Collectors.toList());
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Transactional(readOnly = true)
+    @RequestMapping(method = RequestMethod.GET)
+    @ResponseBody
+    public List<Trip> getTripByQuery(@RequestParam("t_date") String t_date, @RequestParam("t_class") int t_class, @RequestParam("s_location") String start, @RequestParam("e_location") String ends){
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date date;
+        try{
+            date = sdf.parse(t_date);
+            List<TripData> byQuery = tdrep.findByQuery(date,t_class,start,ends);
+            return byQuery.stream().map(this::convertToTrip).collect(Collectors.toList());
+        }catch (ParseException e){
+            e.printStackTrace();
+            return null;
+        }
     }
 }
--- transport-reservation-system/src/main/java/com/example/db/TripDataRepository.java
+++ transport-reservation-system/src/main/java/com/example/db/TripDataRepository.java
@@ -4,14 +4,31 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Date;
 import java.util.List;
 /**
  * Created by ayesh on 9/8/16.
  */
 public interface TripDataRepository extends CrudRepository<TripData, Long> {
-    TripData save(TripData petData);
+    TripData save(TripData td);
 
     TripData findOne(Long aLong);
 
     void delete(Long aLong);
+
+    @Query("select p  from TripData p where p.s_location = :start")
+    List<TripData> findByStart(@Param("start") String start);
+
+    @Query("select p from TripData p where p.e_location = :ends")
+    List<TripData> findByEnd(@Param("ends") String ends);
+
+    @Query("select p from TripData p where p.t_class= :t_class")
+    List<TripData> findByClass(@Param("t_class") int t_class);
+
+    @Query("select p from TripData p where p.t_date = :date")
+    List<TripData> findByDate(@Param("date") Date date);
+
+    @Query("select p from TripData p where p.t_date = :date and p.t_class = :t_class and p.s_location = :start and p.e_location = :ends")
+    List<TripData> findByQuery(@Param("date") Date date,@Param("t_class") int t_class,@Param("start") String start,@Param("ends") String ends);
+
 }
