beb411d20dab81e733312568b13944dbe7a0b7fa
ayesh123 <ayesh9206@gmail.com>
2016-09-08 20:52:58 +0530

    initial-commit

---
+++ transport-reservation-system/src/main/java/com/example/TransportReservationSystemApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportReservationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransportReservationSystemApplication.class, args);
+	}
+}
